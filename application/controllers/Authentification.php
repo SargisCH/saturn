175174a24a586fa812b4bdbd1d3283727d04f140
sargisCH <sargis.chilingaryann@gmail.com>
2017-04-04 20:53:21 +0400

    Registration hash password

--- application/controllers/Authentification.php
+++ application/controllers/Authentification.php
@@ -46,8 +46,8 @@ class Authentification extends CI_Controller {
             $data['password'] = password_hash($this->input->post('password'), PASSWORD_DEFAULT);
             $data['email'] = $this->input->post('email');
             $data['created_at'] = date('y-m-d');
-            $this->load->model('login_and_register_model');
-            $this->login_and_register_model->register($data);
+            $this->load->model('authentification_model');
+            $this->authentification_model->register($data);
             $data_register['success'] = "You are registered";
             $this->load->view('master', $data_register);
         }
